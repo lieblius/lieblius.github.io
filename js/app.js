ad2da93edfac07b04f27f1648d76862436eb99d7
liebler <liebler@umich.edu>
2020-04-04 04:04:54 -0400

    Fixed particles.js mouse tracking and disabled onClick spawning more nodes

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ particlesJS('particles-js',
           "mode": "grab"
         },
         "onclick": {
-          "enable": true,
+          "enable": false,
           "mode": "push"
         },
         "resize": true
